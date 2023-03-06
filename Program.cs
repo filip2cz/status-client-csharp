9084ac53091fbcc9622676ff368aa222d39e2353
filip2cz <filip@fkomarek.eu>
2023-03-06 16:27:44 +0100

    actually sending data

--- Program.cs
+++ Program.cs
@@ -4,23 +4,59 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 
-bool running = true;
-bool debug = true;
+if (debug)
+{
+    Console.WriteLine("Start");
+}
 
+bool debug = true;
+bool auth = false;
+
+if (debug)
+{
+    Console.WriteLine("Reading data from config");
+}
 // nastavení serveru a portu (todo: dát to do config souboru
 string server = "status.fkomarek.eu";
 int port = 35601;
-
 string user = "user";
 string passwd = "password";
 
-while (running)
+while (true)
 {
+    if (debug)
+    {
+        Console.WriteLine("Creating TcpClient instance");
+    }
+    
     // vytvoření instance TCP klienta
-    TcpClient client = new TcpClient(server, port);
+    TcpClient client = new TcpClient();
+
+    if (debug)
+    {
+        Console.WriteLine("Connecting to server...");
+    }
+
+    if (!client.Connected)
+    {
+        client.Connect(server, port);
+        auth = true;
+    }
+
+    if (debug)
+    {
+        if (client.Connected)
+        {
+            Console.WriteLine("Connected");
+        }
+        else if (!client.Connected)
+        {
+            Console.WriteLine("Connection aborted");
+        }
+    }
 
     // získání streamu pro odesílání dat
-    NetworkStream stream = client.GetStream();
+    NetworkStream streamLogin = client.GetStream();
 
     byte[] buffer = new byte[1024];
     int bytesRead = 0;
@@ -36,38 +72,46 @@ while (running)
     }
     */
 
-    // přečte data ze serveru
-    bytesRead = stream.Read(buffer, 0, buffer.Length);
-    Console.WriteLine(Encoding.ASCII.GetString(buffer, 0, bytesRead));
-
-    Thread.Sleep(1000);
-
-    // autentizace
-    string login = $"{user}:{passwd}\r\n";
-    byte[] loginData = Encoding.ASCII.GetBytes(login);
-    if (debug)
+    if (auth)
     {
-        Console.WriteLine($"sending: {login}");
+        // přečte data ze serveru
+        bytesRead = streamLogin.Read(buffer, 0, buffer.Length);
+        Console.WriteLine(Encoding.ASCII.GetString(buffer, 0, bytesRead));
+
+        // autentizace
+        string login = $"{user}:{passwd}\r\n";
+        byte[] loginData = Encoding.ASCII.GetBytes(login);
+        if (debug)
+        {
+            Console.WriteLine($"sending: {login}");
+        }
+        streamLogin.Write(loginData, 0, loginData.Length);
+
+        // přečte data ze serveru
+        bytesRead = streamLogin.Read(buffer, 0, buffer.Length);
+        Console.WriteLine(Encoding.ASCII.GetString(buffer, 0, bytesRead));
+
+        // přečte data ze serveru
+        bytesRead = streamLogin.Read(buffer, 0, buffer.Length);
+        Console.WriteLine(Encoding.ASCII.GetString(buffer, 0, bytesRead));
+
+        auth = false;
     }
-    stream.Write(loginData, 0, loginData.Length);
 
-    // přečte data ze serveru
-    bytesRead = stream.Read(buffer, 0, buffer.Length);
-    Console.WriteLine(Encoding.ASCII.GetString(buffer, 0, bytesRead));
-
-    // přečte data ze serveru
-    bytesRead = stream.Read(buffer, 0, buffer.Length);
-    Console.WriteLine(Encoding.ASCII.GetString(buffer, 0, bytesRead));
-
+    // sending
     while (client.Connected)
     {
-        string data = "lol";
+        string data = "update {\"online6\": false,  \"uptime\": 229, \"load\": 0.93, \"memory_total\": 12200460, \"memory_used\": 2499944, \"swap_total\": 12201980, \"swap_used\": 0, \"hdd_total\": 3761418, \"hdd_used\": 1161319, \"cpu\": 19.0, \"network_rx\": 0, \"network_tx\": 0 }\r\n";
         byte[] dataSend = Encoding.ASCII.GetBytes(data);
+
+        NetworkStream stream = client.GetStream();
         stream.Write(dataSend, 0, dataSend.Length);
-        Thread.Sleep(1000);
+
+        Console.WriteLine(data);
+        Thread.Sleep(3000);
     }
-
+    
+    // disconnect
     client.Close();
-
-    running = false;
+    client.Dispose();
 }
