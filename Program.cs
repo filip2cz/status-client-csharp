c75e64aa81afbcb7af1dd8311ef8f9958c4b9a19
filip2cz <filip@fkomarek.eu>
2023-03-09 09:40:54 +0100

    sending zeros or false on all comming soon things

--- Program.cs
+++ Program.cs
@@ -7,112 +7,121 @@ using Newtonsoft.Json;
 
 bool auth = false;
 
-// nastavení serveru a portu (todo: dát to do config souboru
-var configJson = File.ReadAllText("config.json");
-dynamic config = JsonConvert.DeserializeObject(configJson);
-
-string server = config.server;
-int port = config.port;
-string user = config.user;
-string passwd = config.passwd;
-bool debug = config.debug;
-
-while (true)
+if (File.Exists("config.json") == false)
 {
-    if (debug)
+    Console.WriteLine("config.json not found");
+    return;
+}
+
+else
+{
+    // nastavení serveru a portu (todo: dát to do config souboru
+    var configJson = File.ReadAllText("config.json");
+    dynamic config = JsonConvert.DeserializeObject(configJson);
+
+    string server = config.server;
+    int port = config.port;
+    string user = config.user;
+    string passwd = config.passwd;
+    bool debug = config.debug;
+
+    while (true)
     {
-        Console.WriteLine("Creating TcpClient instance");
-    }
-    
-    // vytvoření instance TCP klienta
-    TcpClient client = new TcpClient();
-
-    if (debug)
-    {
-        Console.WriteLine("Connecting to server...");
-    }
-
-    if (!client.Connected)
-    {
-        client.Connect(server, port);
-        auth = true;
-    }
-
-    if (debug)
-    {
-        if (client.Connected)
-        {
-            Console.WriteLine("Connected");
-        }
-        else if (!client.Connected)
-        {
-            Console.WriteLine("Connection aborted");
-        }
-    }
-
-    // získání streamu pro odesílání dat
-    NetworkStream streamLogin = client.GetStream();
-
-    byte[] buffer = new byte[1024];
-    int bytesRead = 0;
-
-    /*
-    if (client.Connected)
-    {
-        Console.WriteLine("client is still connected btw.");
-    }
-    else
-    {
-        Console.WriteLine("client is not connectd btw.");
-    }
-    */
-
-    if (auth)
-    {
-        // přečte data ze serveru
-        bytesRead = streamLogin.Read(buffer, 0, buffer.Length);
-        Console.WriteLine(Encoding.ASCII.GetString(buffer, 0, bytesRead));
-
-        // autentizace
-        string login = $"{user}:{passwd}\r\n";
-        byte[] loginData = Encoding.ASCII.GetBytes(login);
         if (debug)
         {
-            Console.WriteLine($"sending: {login}");
+            Console.WriteLine("Creating TcpClient instance");
         }
-        streamLogin.Write(loginData, 0, loginData.Length);
 
-        // přečte data ze serveru
-        bytesRead = streamLogin.Read(buffer, 0, buffer.Length);
-        Console.WriteLine(Encoding.ASCII.GetString(buffer, 0, bytesRead));
+        // vytvoření instance TCP klienta
+        TcpClient client = new TcpClient();
 
-        // přečte data ze serveru
-        bytesRead = streamLogin.Read(buffer, 0, buffer.Length);
-        Console.WriteLine(Encoding.ASCII.GetString(buffer, 0, bytesRead));
+        if (debug)
+        {
+            Console.WriteLine("Connecting to server...");
+        }
 
-        auth = false;
-    }
+        if (!client.Connected)
+        {
+            client.Connect(server, port);
+            auth = true;
+        }
 
-    // sending
-    while (client.Connected)
-    {
-        // uptime
-        var uptimeMilliseconds = System.Environment.TickCount64;
-        var uptimeSeconds = (long)(uptimeMilliseconds / 1000);
+        if (debug)
+        {
+            if (client.Connected)
+            {
+                Console.WriteLine("Connected");
+            }
+            else if (!client.Connected)
+            {
+                Console.WriteLine("Connection aborted");
+            }
+        }
+
+        // získání streamu pro odesílání dat
+        NetworkStream streamLogin = client.GetStream();
+
+        byte[] buffer = new byte[1024];
+        int bytesRead = 0;
+
+        /*
+        if (client.Connected)
+        {
+            Console.WriteLine("client is still connected btw.");
+        }
+        else
+        {
+            Console.WriteLine("client is not connectd btw.");
+        }
+        */
+
+        if (auth)
+        {
+            // přečte data ze serveru
+            bytesRead = streamLogin.Read(buffer, 0, buffer.Length);
+            Console.WriteLine(Encoding.ASCII.GetString(buffer, 0, bytesRead));
+
+            // autentizace
+            string login = $"{user}:{passwd}\r\n";
+            byte[] loginData = Encoding.ASCII.GetBytes(login);
+            if (debug)
+            {
+                Console.WriteLine($"sending: {login}");
+            }
+            streamLogin.Write(loginData, 0, loginData.Length);
+
+            // přečte data ze serveru
+            bytesRead = streamLogin.Read(buffer, 0, buffer.Length);
+            Console.WriteLine(Encoding.ASCII.GetString(buffer, 0, bytesRead));
+
+            // přečte data ze serveru
+            bytesRead = streamLogin.Read(buffer, 0, buffer.Length);
+            Console.WriteLine(Encoding.ASCII.GetString(buffer, 0, bytesRead));
+
+            auth = false;
+        }
 
         // sending
+        while (client.Connected)
+        {
+            // uptime
+            var uptimeMilliseconds = System.Environment.TickCount64;
+            var uptimeSeconds = (long)(uptimeMilliseconds / 1000);
 
-        string data = "update {\"online6\": false,  \"uptime\": " + uptimeSeconds.ToString() + ", \"load\": 0.93, \"memory_total\": 12200460, \"memory_used\": 2499944, \"swap_total\": 12201980, \"swap_used\": 0, \"hdd_total\": 3761418, \"hdd_used\": 1161319, \"cpu\": 19.0, \"network_rx\": 0, \"network_tx\": 0 }\r\n";
-        byte[] dataSend = Encoding.ASCII.GetBytes(data);
+            // sending
 
-        NetworkStream stream = client.GetStream();
-        stream.Write(dataSend, 0, dataSend.Length);
+            string data = "update {\"online6\": false,  \"uptime\": " + uptimeSeconds.ToString() + ", \"load\": 0, \"memory_total\": 0, \"memory_used\": 0, \"swap_total\": 0, \"swap_used\": 0, \"hdd_total\": 0, \"hdd_used\": 0, \"cpu\": 0.0, \"network_rx\": 0, \"network_tx\": 0 }\r\n";
+            byte[] dataSend = Encoding.ASCII.GetBytes(data);
 
-        Console.WriteLine(data);
-        Thread.Sleep(3000);
+            NetworkStream stream = client.GetStream();
+            stream.Write(dataSend, 0, dataSend.Length);
+
+            Console.WriteLine(data);
+            Thread.Sleep(3000);
+        }
+
+        // disconnect
+        client.Close();
+        client.Dispose();
     }
-    
-    // disconnect
-    client.Close();
-    client.Dispose();
 }
