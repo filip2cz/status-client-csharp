4261decb158de1fe6670e78cb7879e1c17201308
filip2cz <filip@fkomarek.eu>
2023-03-09 14:52:19 +0100

    better connection checking and catching exceptions

--- Program.cs
+++ Program.cs
@@ -1,14 +1,16 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Net.NetworkInformation;
 using System.Net.Sockets;
 using System.Text;
 using System.Text.Json;
 using Newtonsoft.Json;
 
-bool auth = false;
-
 Console.WriteLine("Status client C# v0.1");
+
+bool auth = false;
+//int pingTries = 0;
 
 if (File.Exists("config.json") == false)
 {
@@ -28,7 +30,7 @@ if (File.Exists("config.json") == false)
     Environment.Exit(0);
 }
 
-// nastavení serveru a portu (todo: dát to do config souboru
+// load config file
 var configJson = File.ReadAllText("config.json");
 dynamic config = JsonConvert.DeserializeObject(configJson);
 
@@ -37,6 +39,41 @@ int port = config.port;
 string user = config.user;
 string passwd = config.passwd;
 bool debug = config.debug;
+//bool pingTest = config.pingtest;
+
+// test if server is avaible with ping
+/*
+if (pingTest)
+{
+    Ping ping = new Ping();
+    PingReply pingReply = ping.Send(server);
+    while (pingReply.Status != IPStatus.Success)
+    {
+        Console.WriteLine(pingTest);
+        Console.WriteLine("Ping test failed, trying again");
+        pingTries++;
+        Thread.Sleep(1000);
+        pingReply = ping.Send(server);
+        if (pingTries > 10)
+        {
+            Console.WriteLine("Ping failed 10 times, exiting...");
+            Environment.Exit(0);
+        }
+    }
+    if (debug)
+    {
+        Console.WriteLine("Ping test was succefull");
+    }
+    pingTries = 0;
+}
+else
+{
+    if (debug)
+    {
+        Console.WriteLine("Skipping ping test, because config.json said");
+    }
+}
+*/
 
 while (true)
 {
@@ -45,57 +82,48 @@ while (true)
         Console.WriteLine("Creating TcpClient instance");
     }
 
-    // vytvoření instance TCP klienta
+    // create TCP Client instance
     TcpClient client = new TcpClient();
 
     if (debug)
-    {
+    {   
         Console.WriteLine("Connecting to server...");
     }
 
-    if (!client.Connected)
+    while (!client.Connected)
     {
-        client.Connect(server, port);
-        auth = true;
-    }
-
-    if (debug)
-    {
-        if (client.Connected)
+        try
         {
-            Console.WriteLine("Connected");
+            client.Connect(server, port);
+            auth = true;
         }
-        else if (!client.Connected)
+        catch (SocketException ex)
         {
-            Console.WriteLine("Connection aborted");
+            if (debug)
+            {
+                Console.WriteLine("SocketException: " + ex.Message);
+                Console.WriteLine("Connection refused, trying again.");
+            }
+            auth = false;
         }
     }
+    
+    Console.WriteLine("Connected");
 
-    // získání streamu pro odesílání dat
+    // get stream for data sending
     NetworkStream streamLogin = client.GetStream();
 
     byte[] buffer = new byte[1024];
     int bytesRead = 0;
 
-    /*
-    if (client.Connected)
-    {
-        Console.WriteLine("client is still connected btw.");
-    }
-    else
-    {
-        Console.WriteLine("client is not connectd btw.");
-    }
-    */
-
     if (auth)
     {
-        // přečte data ze serveru
+        // read data from server
         bytesRead = streamLogin.Read(buffer, 0, buffer.Length);
         Console.WriteLine(Encoding.ASCII.GetString(buffer, 0, bytesRead));
 
-        // autentizace
-        string login = $"{user}:{passwd}\r\n";
+        // autentization
+        string login = $"{config.user}:{config.passwd}\r\n";
         byte[] loginData = Encoding.ASCII.GetBytes(login);
         if (debug)
         {
@@ -103,11 +131,11 @@ while (true)
         }
         streamLogin.Write(loginData, 0, loginData.Length);
 
-        // přečte data ze serveru
+        // read data from server
         bytesRead = streamLogin.Read(buffer, 0, buffer.Length);
         Console.WriteLine(Encoding.ASCII.GetString(buffer, 0, bytesRead));
 
-        // přečte data ze serveru
+        // read data from server
         bytesRead = streamLogin.Read(buffer, 0, buffer.Length);
         Console.WriteLine(Encoding.ASCII.GetString(buffer, 0, bytesRead));
 
@@ -122,18 +150,30 @@ while (true)
         var uptimeSeconds = (long)(uptimeMilliseconds / 1000);
 
         // sending
-
         string data = "update {\"online6\": false,  \"uptime\": " + uptimeSeconds.ToString() + ", \"load\": 0, \"memory_total\": 0, \"memory_used\": 0, \"swap_total\": 0, \"swap_used\": 0, \"hdd_total\": 0, \"hdd_used\": 0, \"cpu\": 0.0, \"network_rx\": 0, \"network_tx\": 0 }\r\n";
         byte[] dataSend = Encoding.ASCII.GetBytes(data);
 
-        NetworkStream stream = client.GetStream();
-        stream.Write(dataSend, 0, dataSend.Length);
-
-        Console.WriteLine(data);
+        try
+        {
+            NetworkStream stream = client.GetStream();
+            stream.Write(dataSend, 0, dataSend.Length);
+        }
+        catch (Exception ex)
+        {
+            if (debug)
+            {
+                Console.WriteLine("Connection refused: " + ex.Message);
+            }
+        }
+        if (debug)
+        {
+            Console.WriteLine(data);
+        }
         Thread.Sleep(3000);
     }
 
     // disconnect
     client.Close();
     client.Dispose();
+    Console.WriteLine("Disconected");
 }
